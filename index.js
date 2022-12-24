9bf02695e00a54811a47d56a1941123f6432c1db
Владислав Маковский <alectorkinga@gmail.com>
2022-12-25 00:52:54 +0200

    add:ipAdress

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 require('dotenv').config();
 var express = require('express');
 var app = express();
-var requestIp = require('request-ip');
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require('cors');
